68e99f81c7f80bd579c1e38a4db586d1366a8697
naniawa <43081953+naniawa@users.noreply.github.com>
2024-01-15 21:26:23 +0900

    Add files via upload

--- markdown_to_pdf.py
+++ markdown_to_pdf.py
@@ -12,25 +12,41 @@ def extract_plantuml_code_from_markdown(input_markdown_path):
     plantuml_matches = re.findall(r"```plantuml(.*?)```", markdown_content, re.DOTALL)
 
     return plantuml_matches
-
+"""
 def generate_diagram_images(plantuml_matches):
     image_paths = []
 
     for i, plantuml_code in enumerate(plantuml_matches, start=1):
         # PlantUMLを画像に変換
-        os.makedirs("images", exist_ok=True)
-        with open(f"images/diagram_{i}.puml", "w", encoding="utf-8") as file:
+        os.makedirs("./", exist_ok=True)
+        with open(f"diagram_{i}.puml", "w", encoding="utf-8") as file:
             file.write(plantuml_code)
 
         #subprocess.run(["plantuml", f"-tpng", f"diagram_{i}.puml"]) #macOS
-        subprocess.run(["plantuml", f"-tsvg", f"images/diagram_{i}.puml"]) #macOS
-        #subprocess.run(["java","-Dfile.encoding=UTF-8", "-jar", "plantuml.jar","-tsvg", f"images/diagram_{i}.puml"]) #windowsOS
+        #subprocess.run(["plantuml", f"-tsvg", f"images/diagram_{i}.puml"]) #macOS
+        subprocess.run(["java","-Dfile.encoding=UTF-8", "-jar", "plantuml.jar","-tsvg", f"diagram_{i}.puml"]) #windowsOS
 
         #image_paths.append(f"diagram_{i}.png")
-        image_paths.append(f"images/diagram_{i}.svg")
-
+        image_paths.append(f"./diagram_{i}.svg")
+        #image_paths.append(os.path.abspath(f"diagram_{i}.svg"))
         # 不要なファイルの削除
         #os.remove(f"diagram_{i}.puml")
+
+    return image_paths
+"""
+def generate_diagram_images(plantuml_matches, output_directory):
+    image_paths = []
+
+    for i, plantuml_code in enumerate(plantuml_matches, start=1):
+        # PlantUMLを画像に変換
+        os.makedirs(output_directory, exist_ok=True)
+        output_path = os.path.abspath(os.path.join(output_directory, f"diagram_{i}.puml"))
+        with open(output_path, "w", encoding="utf-8") as file:
+            file.write(plantuml_code)
+
+        subprocess.run(["java", "-Dfile.encoding=UTF-8", "-jar", "plantuml.jar", "-tsvg", output_path])
+
+        image_paths.append(os.path.abspath(f"./diagram_{i}.svg"))
 
     return image_paths
 
@@ -51,22 +67,44 @@ def save_markdown_to_file(markdown_content, output_md_path):
 
 def convert_md_to_pdf(input_md_path,):
     try:
-        subprocess.run(["md-to-pdf", input_md_path], check=True)
+        md_to_pdf_path = os.path.join(os.environ['APPDATA'], 'npm', 'md-to-pdf.cmd')
+        #subprocess.run(["md-to-pdf", input_md_path], check=True)
+        subprocess.run([md_to_pdf_path, input_md_path], check=True)
         print("Conversion successful!")
     except subprocess.CalledProcessError as e:
         print(f"Error during conversion: {e}")
-
+"""
 # 外部で指定されたMarkdownファイルと出力PDFファイルのパス
 print("input file(.md)")
 input_markdown_path = input()  # ユーザーからの入力を受け取る
-#print("output file(.md)")
+print("output file(.md)")
 #output_md_path = input()
-output_md_path = "output.md"
-#/Users/nishikawashota/Downloads/Markdown/out.md
-
+#output_md_path = "output.md"
+output_md_path = input()
 
 plantuml_matches = extract_plantuml_code_from_markdown(input_markdown_path)
 image_paths = generate_diagram_images(plantuml_matches)
+markdown_content = insert_diagrams_into_markdown(input_markdown_path, image_paths)
+
+# 生成されたMarkdownをファイルに保存
+save_markdown_to_file(markdown_content, output_md_path)
+
+# 保存したMarkdownをPDFに変換
+convert_md_to_pdf(output_md_path)
+"""
+
+# 外部で指定されたMarkdownファイルと出力PDFファイルのパス
+print("Input Markdown file (.md):")
+input_markdown_path = input()
+
+print("Output directory for images:")
+output_directory = input()
+
+print("Output Markdown file (.md):")
+output_md_path = input()
+
+plantuml_matches = extract_plantuml_code_from_markdown(input_markdown_path)
+image_paths = generate_diagram_images(plantuml_matches, output_directory)
 markdown_content = insert_diagrams_into_markdown(input_markdown_path, image_paths)
 
 # 生成されたMarkdownをファイルに保存
